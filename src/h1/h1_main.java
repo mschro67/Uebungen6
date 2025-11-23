7ced3c9bf77afddba2f33c8b300b1ea9ad77848a
maximilian107 <maximilian.s107@evt.schule.koeln>
2025-11-23 09:17:30 +0100

    Add H1-H3

---
+++ src/h1/h1_main.java
@@ -0,0 +1,25 @@
+//mschro67
+
+package h1;
+
+import java.util.Arrays;
+
+public class h1_main{
+    public static boolean isMirrorArray(int[] a, int[] b){
+        boolean result=true;
+        if (a.length==b.length){
+            for (int x=0;x<a.length;x++){
+                if (a[x]!=b[b.length-1-x]){
+                    return false;
+                }
+            }
+        }else{
+            result=false;
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        int[] a={0,1,2,3},b={3,2,1,0};
+        System.out.println("a: "+Arrays.toString(a)+"\nb: "+Arrays.toString(b)+"\nisMirrorArray: "+isMirrorArray(a,b));
+    }
+}
---
+++ src/h2/h2_main.java
@@ -0,0 +1,25 @@
+//by mschro67
+
+package h2;
+
+import java.util.Arrays;
+
+public class h2_main{
+    public static boolean compareArrays(int[] a,int[] b){
+        boolean result=true;
+        if (a.length==b.length) {
+            for (int x = 0; x<a.length;x++){
+                if (Integer.valueOf(a[x])!=Integer.valueOf(b[x])){
+                    return false;
+                }
+            }
+        }else{
+            result=false;
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        int[] a={0,1,2,3},b={1,1,2,3};
+        System.out.println("a: "+Arrays.toString(a)+"\nb: "+Arrays.toString(b)+"\ncomparearrays: "+compareArrays(a,b));
+    }
+}
