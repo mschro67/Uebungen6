4b0aad298d4ee28677025afa372d38c45ad9e929
Maxi <maximilian.s107@evt.schule.koeln>
2025-11-27 16:29:24 +0000

    Add files via upload

--- src/p3/p3_main.java
+++ src/p3/p3_main.java
@@ -6,18 +6,19 @@ import java.util.Arrays;
 
 public class p3_main{
     public static int methode(int[] a) {
-        int max = -1;
+        int[] big={Integer.valueOf(a[0]),Integer.valueOf(a[0])};
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a.length; j++) {
                 if (a[j] > a[i]) {
-                    max = a[j];
+		    big[1] = Integer.valueOf(big[0]);
+                    big[0] = Integer.valueOf(Integer.valueOf(a[j]));
                 }
             }
         }
-        return max;
+        return big[1];
     }
     public static void main(String[] args){
         int[] a={1,2,3,4};
-        System.out.println("a: "+Arrays.toString(a)+"\nmax: "+methode(a));
+        System.out.println("a: "+Arrays.toString(a)+"\nmethode: "+methode(a));
     }
 }
